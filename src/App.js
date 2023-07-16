b910d25e70dcf768b6ee42bfaf90bbd63489cc53
mikachou <chou1005.tw@gmail.com>
2023-07-16 11:55:16 +0800

    first upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  let [name, setName] = useState();
+  let [email, setEmail] = useState();
+
+  function onFormSubmit(event) {
+    event.preventDefault();
+    setName(event.target.name.value);
+    setEmail(event.target.email.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="formWrapper">
+        <form onSubmit={onFormSubmit}>
+          <h3>My First React Form</h3>
+
+          <div className="nameBox">
+            <label htmlFor="name">Name:</label>
+            <input name="name" id="name" type="text" />
+          </div>
+
+          <div className="emailBox">
+            <label htmlFor="email">Email:</label>
+            <input name="email" id="email" type="email" />
+          </div>
+          <button className="btn" type="submit">
+            Submit
+          </button>
+        </form>
+        <div className="showWrapper">
+          <h4>You have inserted as below:</h4>
+          <ul>
+            <li> {name}</li>
+            <li>{email}</li>
+          </ul>
+        </div>
+      </div>
+    </>
   );
 }
 
